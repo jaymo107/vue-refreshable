47907a0bad9d12cbe70184f761f1c4c656095884
James Davies <jaymo107@gmail.com>
2021-07-26 13:09:16 +0100

    Add lodash

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,25 @@
 import { terser } from "rollup-plugin-terser";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
     input: 'src/main.js',
     output: {
         name: 'VueRefreshable',
         file: 'dist/vueRefreshable.js',
-        exports: 'named'
+        exports: 'named',
+        sourcemap: true
     },
     watch: {
         include: 'src/**/*',
         exclude: 'node_modules/**',
     },
     plugins: [
-        terser()
+        terser(),
+        commonjs({
+            include: /node_modules/
+        }),
+        nodeResolve(),
     ]
   };
   
